707fb1d211128b266e9f4affd193e216244209a8
Riku Taneda <riku.program@gmail.com>
2018-10-20 17:02:56 +0900

    set topics migration file

--- db/migrate/20181020075915_create_topics.rb
+++ db/migrate/20181020075915_create_topics.rb
@@ -1,7 +1,8 @@
 class CreateTopics < ActiveRecord::Migration[5.0]
   def change
     create_table :topics do |t|
-
+      t.string       :topic,  null: false
+      t.integer      :taiketsu_id, null: false
       t.timestamps
     end
   end
