a8047073e3925032a89b5a05adb65ce5c078a197
Riku Taneda <riku.program@gmail.com>
2018-10-20 17:24:28 +0900

    set topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,7 +7,17 @@ class TopicsController < ApplicationController
     end
 
     def create
-
+      @topic = Topic.new(topic_params)
+      if @topic.save
+      redirect_to topic
+      else
+        render :back
+      end
     end
 
+    private
+
+    def topic_params
+
+    end
 end
