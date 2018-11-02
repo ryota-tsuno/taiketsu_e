8c41eb57bcb78a1d053f30afe7a0f63222749ba2
津野亮太 <a_bc_j1024@icloud.com>
2018-11-02 15:45:25 +0900

    comment_form（仮）

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,7 @@
 class Topic < ApplicationRecord
   belongs_to  :taiketsu, optional: true
+  has_many :comments, dependent: :destroy
+  # 投稿が削除されたときに一緒にコメントも削除する
   validates   :topic,
       presence: true,
       length:   {in: 1 .. 50}
