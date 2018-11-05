c4e2b9801671f688d0c199ca84fd7dc4ee6c3119
nomuyo <goosehouse.3963@gmail.com>
2018-11-05 20:26:13 +0900

    Add column  session_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181105060127) do
+ActiveRecord::Schema.define(version: 20181105111519) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535, null: false
