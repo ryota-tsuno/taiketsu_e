2172c3858f2b73130a40797d939729e0159eb42d
Riku Taneda <riku.program@gmail.com>
2018-11-05 14:26:20 +0900

    set routing taiketsu destory

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "taiketsus#index"
-  resources :taiketsus, only: [:index, :show, :create] do
+  resources :taiketsus, only: [:index, :show, :create, :destory] do
     resources :comments, only: [:create]
   end
   resources :abouts, only: [:index]
