125747357b161d1f7632d25389b50bc4527d2bac
津野亮太 <a_bc_j1024@icloud.com>
2018-11-08 15:08:20 +0900

    delete_migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106015855) do
+ActiveRecord::Schema.define(version: 20181105095404) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535, null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20181106015855) do
   create_table "topics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "topic",       null: false
     t.integer  "taiketsu_id", null: false
+    t.string   "session_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
