2adeaeae260fa12ab8aa5f8f8c1f121884a8287c
津野亮太 <a_bc_j1024@icloud.com>
2018-11-14 15:37:41 +0900

    add_taiketsu_effects

---
+++ app/assets/javascripts/taiketsu_effect.js
@@ -0,0 +1,48 @@
+$(function (){
+    var taiketsu_red = '#FF0033'
+    var taiketsu_yellow = '#FFFF00'
+    $('.btn1').on('click', function(){
+        $(this).prop("disabled", true);
+        $(this).remove('');
+        $(this).css({
+            'background-color' : taiketsu_red,
+            'color' : taiketsu_yellow,
+        })
+        $('.btn').css({
+            'background-color' : taiketsu_red,
+            'color' : taiketsu_yellow
+        })
+        $('.header, .footer').css({
+            'background-image' : 'url(./images/giphy.gif)',
+        });
+        $('.main').css({
+            'background-image' : 'url(./images/boxing.jpg)',
+            'background-repeat' : 'none',
+        })
+        $('.header__middle--button').prepend("<img src = './images/jhonson.png' width = '200' height = '300'>");
+        $('.header__middle--button').append("<img src = './images/resura.png' width = '200' height = '300'>");
+        $('.header__middle--button').css({
+            'width' : '600px',
+        })
+        $('.header__middle--sentence').css({
+            'color' : taiketsu_red,
+            'font-weight' : 'bold', 
+            'font-size' : '30px',
+            'text-shadow' : '8px 5px 5px #220000'
+        });
+        $('.icon').css({
+            'color' : 'black',
+            'border' : 'solid 1px black',
+        });
+        $('.popular_title, .main__name--bottom').css({
+            'color' : taiketsu_red,
+            'font-weight' : 'bold', 
+            'text-shadow' : '8px 5px 5px #220000'
+        });
+        $('.footer__title, .footer__sentence, .footer__form--input__VS, .footer__form--sentence').css({
+            'color' : taiketsu_red,
+            'font-weight' : 'bold', 
+            'text-shadow' : '8px 5px 5px #220000'
+        });
+    });
+})
